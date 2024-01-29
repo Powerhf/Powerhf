03618a8eab5df78d35929364274734744e4565d1
powerhf <ituser1999@gmail.com>
2024-01-29 17:43:55 +0530

    second commit

--- powerhf/app/views.py
+++ powerhf/app/views.py
@@ -7,7 +7,6 @@ from django.contrib.auth.views import LogoutView
 from django.contrib import messages
 from django.views.generic import TemplateView
 from django.core.paginator import Paginator
-import pandas as pd
 
 
 
