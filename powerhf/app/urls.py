593ef967e2f20d7c7a25263a0b502531549e6427
powerhf <ituser1999@gmail.com>
2024-02-01 15:37:25 +0530

    01 feb 24

--- powerhf/app/urls.py
+++ powerhf/app/urls.py
@@ -2,15 +2,16 @@ from django.urls import path
 from app.views import *
 
 urlpatterns = [
+    # Dashboard:
     path('', Index, name='index'),
 
     # Reports:
-    path('reports/hoto-report/', Reports_Hoto, name='hoto_report'),
-    
+    path('reports/hoto-report/', Reports_Hoto, name='hoto_report'),    
     path('reports/energy-drf/', DRFReport.as_view(), name='atc_site_report'),
     path('reports/energy-fuel-drawn/', FuelDrawnReport.as_view(), name='fuel_drawn'),
     path('reports/energy-energy-reading/', EnergyReadingReport.as_view(), name='diesel_filling'),
 
+    # Filtering Reports:
     path('reports/energy-dfr-filter/', EnergyDFRFilters.as_view(), name='energy_dfr_filter'),
     path('reports/energy-reading-filter/', EnergyReadingFilters.as_view(), name='energy_reading_filter'),
     path('reports/fuel-drawn-filter/', FuelDrawnFilters.as_view(), name='fuel_drawn_filter'),
@@ -19,5 +20,6 @@ urlpatterns = [
     path('forms/diesel-meter/filling-reading-fill-form/', DieselFillingOrReadingViews.as_view(), name='atcform'),
     path('forms/fuel-drawn-fill-form/', FuelDrawnViews.as_view(), name='fueldrawnform'),
 
+    # Logout:
     path('user/logout/', LogOut.as_view(), name='logout'),
 ]
